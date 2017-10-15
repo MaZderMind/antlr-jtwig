f33a3c4b2f0aaad703b7efe81b41723027992011
MaZderMind <git@mazdermind.de>
2017-10-15 15:42:49 +0200

    implement testrig with maven

--- test-rig.sh
+++ test-rig.sh
@@ -5,7 +5,4 @@
 #  echo '(5+3)*9' | ./test-rig.sh expression -gui
 #
 
-mvn compile
-export CLASSPATH="./target/classes:$HOME/.m2/repository/org/antlr/antlr4-runtime/4.7/antlr4-runtime-4.7.jar:$CLASSPATH"
-echo $CLASSPATH
-java org.antlr.v4.gui.TestRig de.mazdermind.playground.antlrtwig.grammar.Twig "$@"
+mvn compile exec:java -Dexec.mainClass="org.antlr.v4.gui.TestRig" -Dexec.args="de.mazdermind.playground.antlrtwig.grammar.Twig $*"
